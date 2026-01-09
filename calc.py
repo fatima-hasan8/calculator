cc3bcde9183cac1c2cc77b93ea2288ce8cacd346
fatima-hasan8 <fh08026@st.habib.edu.pk>
2026-01-09 13:54:11 +0500

    multiplication added

--- calc.py
+++ calc.py
@@ -1,1 +1,13 @@
-print("DESTROYNG THIS CODE HAHAH")
+# Version 3: Added multiplication
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+print("2 + 2 =", add(2, 2))
+print("5 - 3 =", subtract(5, 3))
+print("10 * 2 =", multiply(10, 2))
