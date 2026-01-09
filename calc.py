75deb946f69a39a79eaad211b2feac51f00cae0c
fatima-hasan8 <fh08026@st.habib.edu.pk>
2026-01-09 13:50:01 +0500

    broekn code

--- calc.py
+++ calc.py
@@ -1,12 +1,1 @@
-def add(a,b):
-    return a + b
-
-print("2 + 2 =", add(2, 2))
-
-print("4 + 4 =", add(4, 4))
-
-def subtract(a, b):
-    return a - b
-
-print("2 + 2 =", add(2, 2))
-print("5 - 3 =", subtract(5, 3))
+print("DESTROYNG THIS CODE HAHAH")
