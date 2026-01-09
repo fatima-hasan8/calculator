53c06c64894d4f7594a2ba80bbd1b5dca5c892ca
fatima-hasan8 <fh08026@st.habib.edu.pk>
2026-01-09 14:41:18 +0500

    division now added

--- calc.py
+++ calc.py
@@ -10,4 +10,11 @@ def multiply(a, b):
 
 print("2 + 2 =", add(2, 2))
 print("5 - 3 =", subtract(5, 3))
-print("10 * 2 =", multiply(10, 2))
+print("10 * 2 =", multiply(10, 5))
+
+def divide(a, b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
+
+print("10 / 2 =", divide(10, 2))
